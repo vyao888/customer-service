6f7ff973840e8bcabc6bfdc90966a05980ffa950
vyao888 <vyao888@hotmail.co.uk>
2021-02-23 17:42:59 +0000

    initial setup

---
+++ src/test/java/com/hsbc/task/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsbc.task.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
